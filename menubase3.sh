46fba5759f6fc2f595d0406ed2570332bd8dd420
NEBULA1978 <rpascualey@gmail.com>
2023-03-11 02:32:17 +0100

    Corrigiendo menubase3.sh añadiendo opcion

--- menubase3.sh
+++ menubase3.sh
@@ -46,28 +46,57 @@ elif [ $accion -eq 2 ]; then
   fi
 
 elif [ $accion -eq 3 ]; then
-  ls
-  # Preguntar al usuario qué carpeta desea entrar y qué archivo desea mover
-  echo "Introduce el nombre de la carpeta a la que deseas entrar:"
-  
-  read carpeta
-  cd $carpeta
-  echo "Introduce el nombre del archivo que deseas mover:"
-  ls
-  read archivo
-  echo "Introduce el nombre de la carpeta de destino:"
-  cd ..
-  ls
-  read destino
-  # Verificar si el archivo existe
-  if [ -f $carpeta/$archivo ]; then
-    # Mover el archivo
-    mv $carpeta/$archivo $destino
-    echo "El archivo $archivo ha sido movido a $destino."
+  echo "Rutas absolutas disponibles:"
+  echo "1. Home ($HOME)"
+  echo "2. Directorio actual ($PWD)"
+  echo "Introduce el número de la ruta absoluta que deseas utilizar como carpeta inicial:"
+  read ruta
+  if [ $ruta -eq 1 ]; then
+    carpeta_inicial="$HOME"
   else
-    # Mostrar un mensaje de error si el archivo no existe
-    echo "El archivo $carpeta/$archivo no existe."
+    carpeta_inicial="$PWD"
   fi
+  cd $carpeta_inicial
+  echo "Estás en la carpeta $carpeta_inicial"
+  while :
+  do
+    ls
+    echo "Introduce el nombre de la carpeta a la que deseas entrar (Q/q para salir):"
+    read carpeta
+    if [[ $carpeta == "Q" || $carpeta == "q" ]]; then
+      break
+    elif [ -d $carpeta ]; then
+      cd $carpeta
+      echo "Estás en la carpeta $carpeta"
+    else
+      echo "La carpeta $carpeta no existe."
+    fi
+    echo "¿Deseas seleccionar un archivo o entrar en una carpeta? (A/C) (Q/q para retroceder):"
+    read opcion
+    if [[ $opcion == "A" || $opcion == "a" ]]; then
+      echo "Introduce el nombre del archivo que deseas mover (Q/q para retroceder):"
+      read archivo
+      if [[ $archivo == "Q" || $archivo == "q" ]]; then
+        cd ..
+      elif [ -f $carpeta/$archivo ]; then
+        echo "Introduce el nombre de la carpeta de destino:"
+        cd ..
+        ls
+        read destino
+        mv $carpeta/$archivo $destino
+        echo "El archivo $archivo ha sido movido a $destino."
+        break
+      else
+        echo "El archivo $carpeta/$archivo no existe."
+      fi
+    elif [[ $opcion == "C" || $opcion == "c" ]]; then
+      continue
+    else
+      echo "Opción no válida."
+    fi
+  done
+
+
 
 else
   # Mostrar un mensaje de error si la acción no es válida
