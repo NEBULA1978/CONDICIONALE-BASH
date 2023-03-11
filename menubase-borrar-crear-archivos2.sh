cdbba0057eeb520bbd35a37fbb19ab63713228f9
NEBULA1978 <rpascualey@gmail.com>
2023-03-11 03:35:44 +0100

    Practicando condicionales4 bash

--- menubase-borrar-crear-archivos2.sh
+++ menubase-borrar-crear-archivos2.sh
@@ -28,6 +28,7 @@ rm "$letter"?.txt
 
 
 echo "//////////////////////////////"
+
 echo "Borrar  los archivos que empiecen por fichero seguido de un guion con 1 caracter y termine con .txt"
 # rm *-?.txt
 echo "Introduce el prefijo de los archivos que quieres borrar (ej. fichero-):"
@@ -37,6 +38,29 @@ read num_chars
 
 rm "$prefix"-?.txt
 
-De esta manera, el script pedirá al usuario que introduzca el prefijo y el número de caracteres después del guion antes de ejecutar el comando rm.
+# De esta manera, l script pedirá al usuario que introduzca el prefijo y el número de caracteres después del guion antes de ejecutar el comando rm.
 
+# 
 
+echo "//////////////////////////////"
+
+# Para que el usuario pueda introducir los valores, puedes usar el comando read para leer la entrada del usuario y almacenarla en una variable. Luego, puedes utilizar esa variable en el comando rm. Aquí hay un ejemplo:
+
+echo "Introduce la letra (A o B) que quieres usar en los archivos fichero[AB].txt"
+read letter
+rm fichero$letter.txt
+
+echo "//////////////////////////////"
+
+ echo "Queremos borrar la palabra que contenga fichero y despues no termine ni con A o B y termine con .txt"
+        echo "Solo borra los ficheros que no sean ficheroA.txt ficheroB.txt"
+        # rm fichero[^AB].txt
+
+#!/bin/bash
+
+echo "Introduce la palabra fichero: "
+read nombre
+echo "Introduce A o B: "
+read terminacion
+
+rm ${nombre}[^${terminacion}].txt
